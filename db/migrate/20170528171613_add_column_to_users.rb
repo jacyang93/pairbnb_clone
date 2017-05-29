d91572d945c29fb4bf4c3b83faf3afba5df89f67
Jacy <Jacyang93@gmail.com>
2017-05-29 12:10:49 +0800

    added few columns for registration forms

---
+++ db/migrate/20170528171613_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :gender, :string
+    add_column :users, :age, :integer
+    add_column :users, :birthday, :date
+  end
+end
