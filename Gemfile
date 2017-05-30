a1479c2c988e2f65ea92e3b08b7ca2772230d431
Jacy <Jacyang93@gmail.com>
2017-05-30 14:44:53 +0800

    facebook auth with omniauth gem

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ end
 
 gem 'clearance'
 gem "figaro"
+gem 'omniauth-facebook'
+gem 'omniauth'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.3'
 # Use postgresql as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
   include Clearance::User
+  has_many :authentications, :dependent => :destroy
+
+  def self.create_with_auth_and_hash(authentication, auth_hash)
+      user = User.create!(name: auth_hash["extra"]["raw_info"]["name"], email: auth_hash["extra"]["raw_info"]["email"], age: auth_hash["extra"]["raw_info"]["age_range"].min[1], birthday: auth_hash["extra"]["raw_info"]["birthday"], gender:  auth_hash["extra"]["raw_info"]["gender"])
+      user.authentications << (authentication)
+      return user
+  end
+
+  def fb_token
+    x = self.authentications.where(:provider => :facebook).first
+    return x.token unless x.nil?
+  end
+
+  def password_optional?
+    true
+  end
 end
